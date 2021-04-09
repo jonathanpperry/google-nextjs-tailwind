e61118d4e1a0e31d73b68d96e656b2eba6239bc3
Jonathan Perry <jpper@umich.edu>
2021-04-09 22:57:19 +0900

    Finished through 1 hour

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
 import Image from "next/image";
 
 export default function Home() {
@@ -36,9 +37,14 @@ export default function Home() {
       <form>
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-          height="100"
-          width="300"
+          height={100}
+          width={300}
         />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline=none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
       </form>
 
       {/* Footer */}
